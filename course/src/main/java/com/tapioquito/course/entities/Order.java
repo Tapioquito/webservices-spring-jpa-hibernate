e16e4a3c1113927f298d966a455538ba2aa1d124
tapioquito <caioportoramos@gmail.com>
2024-03-31 17:13:43 -0300

    Enum OrderStatus, Category

--- course/src/main/java/com/tapioquito/course/entities/Order.java
+++ course/src/main/java/com/tapioquito/course/entities/Order.java
@@ -5,7 +5,8 @@ import java.io.Serializable;
 import java.time.Instant;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
+
+import com.tapioquito.course.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -31,15 +32,16 @@ public class Order implements Serializable {
     @JoinColumn(name = "client_id")
     private User client;
 
-    // private OrderStatus orderStatus;
+    private Integer orderStatus;
 
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         super();
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -57,6 +59,15 @@ public class Order implements Serializable {
 
     public Instant getMoment() {
         return moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null)
+            this.orderStatus = orderStatus.getCode();
     }
 
     public void setMoment(Instant moment) {
